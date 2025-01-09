bfa114fb343f7ed86d35fb32574d93ead9f98519
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2025-01-09 19:12:48 +0530

    fixing some erros

--- backend/DataBase.js
+++ backend/DataBase.js
@@ -2,7 +2,7 @@ const mongoose  = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(" mongodb+srv://ankushcoder497001:<db_password>@websitegenerate.kj83h.mongodb.net/?retryWrites=true&w=majority&appName=WebsiteGenerate")
+mongoose.connect("mongodb+srv://ankushcoder497001:lERzR7GstH1VNRbp@cluster0.i25wd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
   console.log("Connected to MongoDB");
 })
